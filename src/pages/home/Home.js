36ad36c1199014fe38d3fee4f6648d52960cbbb1
Sergio <caue.sergio@gail.com>
2022-05-10 08:38:45 -0300

    commit consumo

---
+++ src/pages/home/Home.js
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import ListaProdutos from "../../components/listaProdutos/ListaProdutos";
+
+function Home () {
+
+    const [produtos, setProdutos] = useState([])
+    
+    useEffect(() => {
+    axios.get('http://localhost:3001/produtos/')
+    .then((response)=> {
+        setProdutos(response.data)
+    })
+    .catch((err)=> {
+        console.error("Ops! ocorreu um erro" + err)
+    })}, [])
+
+    return (
+        <div>
+            <h1>Home</h1>
+            <ListaProdutos produtos={produtos} />
+        </div>
+    );
+  }
+  
+  export default Home;
